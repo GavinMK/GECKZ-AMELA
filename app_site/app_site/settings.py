6b1306d9f6fea239a8c2e696070217ca4793592f
z-ach <zachkaufman97@gmail.com>
2019-02-28 17:41:07 -0800

    changed to mysql db, added git ignore for .pyc files

--- app_site/app_site/settings.py
+++ app_site/app_site/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app_site',
+		'USER': 'projectuser',
+		'PASSWORD': 'password',
+		'HOST': 'localhost',
+		'PORT': '',
     }
 }
 
