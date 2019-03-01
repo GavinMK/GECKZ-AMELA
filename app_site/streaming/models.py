91536594a1d12a5d38140db4ccd1d7eacdb58541
z-ach <zachkaufman97@gmail.com>
2019-03-01 12:49:42 -0800

    added preferences, tv show, tv season, and tv episode as models

--- app_site/streaming/models.py
+++ app_site/streaming/models.py
@@ -20,6 +20,30 @@ class User(models.Model):
     username = models.CharField(max_length=15, primary_key=True, null=False)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    #password?
+    password = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
     last_login = models.DateTimeField()
+
+class Preferences(models.Model):
+    username = models.CharField(max_length=15, primary_key=True, null=False)
+    email_opt_in = models.BooleanField(default=True)
+
+
+class TV_Show(models.Model):
+    tv_show_id = models.IntegerField(primary_key=True)
+    num_seasons = models.IntegerField()
+
+class TV_Season(models.Model):
+    tv_show_id = models.IntegerField(primary_key=True)
+    season_number = models.IntegerField()
+    num_episodes = models.IntegerField()
+    description = models.TextField()
+    year = models.IntegerField()
+
+class TV_Episode(models.Model):
+    tv_episode_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    air_date = models.DateField()
+    episode_number = models.IntegerField()
+    runtime = models.TimeField() #may be better way to store this.. unsure
