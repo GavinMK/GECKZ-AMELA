ab31f11fc05d55dd58e9c506c077bb5fc1c6470f
BabaLala 😊 <babalala@AJ.fios-router.home>
2019-03-09 22:19:14 -0800

    login page and other minor changes

--- app_site/streaming/urls.py
+++ app_site/streaming/urls.py
@@ -6,4 +6,5 @@ app_name = 'streaming'
 urlpatterns = [
     path('', views.index, name='index'),
     path('createuser/', views.create_user_page, name='createUser'),
+    path('login/', views.login, name='login'),
 ]
