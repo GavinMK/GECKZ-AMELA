48005735f470a775cbb707b73f08107a6bcc4114
Kenny Haworth <kehaworth@cpp.edu>
2019-03-12 10:46:43 -0700

    Now uses non-deprecated code

--- app_site/app_site/views.py
+++ app_site/app_site/views.py
@@ -1,7 +1,7 @@
 #This file redirects our site-wide homepage to an app directory(currently /streaming/)
 #Thus, it does not belong in an app such as streaming
 
-from django.shortcuts import redirect
+from django.http import HttpResponsePermanentRedirect
 
 def redirect_root(request):
-    return redirect('/streaming/', permanent=True) #permanent redirect
+    return HttpResponsePermanentRedirect('/streaming/', permanent=True) #permanent redirect
