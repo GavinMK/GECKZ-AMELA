201e3284e4641811f287c7ef4d6559ebc12f07d7
GavinMK <gavinkgrn@gmail.com>
2019-04-09 17:32:12 -0700

    added automation for billing cycles. Charge function not implemented

---
+++ app_site/streaming/management/commands/check_subs.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+from streaming.models import *
+from datetime import *
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for billing in Billing.objects.all():
+            if billing.next_payment_date > datetime.now().date():
+                billing.charge()
+
+
