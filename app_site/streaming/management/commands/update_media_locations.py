2b0e40e518dcd2ac5ace69fa3a077641222bd526
z-ach <zachkaufman97@gmail.com>
2019-04-22 11:10:32 -0700

    media update command with example json file

--- app_site/streaming/management/commands/update_media_locations.py
+++ app_site/streaming/management/commands/update_media_locations.py
@@ -10,7 +10,12 @@ class Command(BaseCommand):
         data_extractor = re.compile(r"Episode\s(\d+)\s([^\n]*)")
         with open('playlists.json') as input_file:
             data = json.load(input_file)
-            for show, episodes in data.items():
+            for title, playlist in data["Movies"].items():
+                movie = Movie.objects.get(title=title)
+                movie.file_location = playlist
+                movie.save()
+                print(movie, "successfully updated to playlist ID", movie.file_location)
+            for show, episodes in data["Shows"].items():
                 print("Updating show: ", show)
                 for title, playlist in episodes.items():
                     matches = data_extractor.search(title)
@@ -18,4 +23,4 @@ class Command(BaseCommand):
                         tv = TVEpisode.objects.get(title=matches.group(2), episode_number=matches.group(1))
                         tv.file_location = playlist
                         tv.save()
-                        print("\t", tv, "successfully updated to playlist ID", playlist)
+                        print("\t", tv, "successfully updated to playlist ID", tv.file_location)
