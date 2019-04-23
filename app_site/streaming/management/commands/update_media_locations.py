d508d6bf5e51ded9bf78f3b4f2dba897f4bc3104
z-ach <zachkaufman97@gmail.com>
2019-04-22 17:55:09 -0700

    added master json file, updated jwplayer js, command is now fully ready

--- app_site/streaming/management/commands/update_media_locations.py
+++ app_site/streaming/management/commands/update_media_locations.py
@@ -10,17 +10,19 @@ class Command(BaseCommand):
         data_extractor = re.compile(r"Episode\s(\d+)\s([^\n]*)")
         with open('playlists.json') as input_file:
             data = json.load(input_file)
-            for title, playlist in data["Movies"].items():
-                movie = Movie.objects.get(title=title)
-                movie.file_location = playlist
-                movie.save()
-                print(movie, "successfully updated to playlist ID", movie.file_location)
-            for show, episodes in data["Shows"].items():
-                print("Updating show: ", show)
-                for title, playlist in episodes.items():
-                    matches = data_extractor.search(title)
-                    if matches:
-                        tv = TVEpisode.objects.get(title=matches.group(2), episode_number=matches.group(1))
-                        tv.file_location = playlist
-                        tv.save()
-                        print("\t", tv, "successfully updated to playlist ID", tv.file_location)
+            if "Movies" in data:
+                for title, playlist in data["Movies"].items():
+                    movie = Movie.objects.get(title=title)
+                    movie.file_location = playlist
+                    movie.save()
+                    print(movie, "successfully updated to playlist ID", movie.file_location)
+            if "Shows" in data:
+                for show, episodes in data["Shows"].items():
+                    print("Updating show: ", show)
+                    for title, playlist in episodes.items():
+                        matches = data_extractor.search(title)
+                        if matches:
+                            tv = TVEpisode.objects.get(title=matches.group(2), episode_number=matches.group(1))
+                            tv.file_location = playlist
+                            tv.save()
+                            print("\t", tv, "successfully updated to playlist ID", tv.file_location)
