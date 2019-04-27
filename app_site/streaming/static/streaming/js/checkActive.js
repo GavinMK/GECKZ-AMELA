1ed732e949748851bcf36cfbb43560f4c210de02
BabaLala 😊 <babalala@AJ.local>
2019-04-26 19:46:07 -0700

    inbox nav highlighting works again

--- app_site/streaming/static/streaming/js/checkActive.js
+++ app_site/streaming/static/streaming/js/checkActive.js
@@ -1,2 +1,9 @@
 let id = location.pathname.split("/")[2];
 document.getElementById(id).classList.add("active");
+if(id === "inbox"){
+    let id2 = location.pathname.split("/")[3];
+    if(id2.includes("Inbox"))
+        document.getElementById(id2).classList.add("active");
+    else
+        document.getElementById("unreadInbox").classList.add("active");
+}
